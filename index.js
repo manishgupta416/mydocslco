4aa82bc91033e213be88b2078b7c9d65eb57d3c4
Manish Gupta <manish.info2020@gmail.com>
2022-01-15 12:10:09 +0530

    string based get request

--- index.js
+++ index.js
@@ -2,14 +2,36 @@ const express = require("express");
 
 const app = express();
 
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+let courses = [
+  {
+    id: "11",
+    name: "Learn ReactJs",
+    price: 299,
+  },
+  {
+    id: "12",
+    name: "Learn EcpressJs",
+    price: 599,
+  },
+  {
+    id: "11",
+    name: "Learn NodeJs",
+    price: 699,
+  },
+];
 
 app.get("/", (req, res) => {
   res.send("hello from lco");
+});
+
+app.get("/api/v1/lco", (req, res) => {
+  res.send("hello from lco docs");
 });
 
 app.listen(4000, () => {
